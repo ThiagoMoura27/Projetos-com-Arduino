714e136ab62cbc220a2eaa00ef981b661c90b90c
Thiago Moura <thiago.uapi@gmail.com>
2018-02-01 10:41:41 -0300

    Add files via upload

---
+++ Lab_temperatura.ino
@@ -0,0 +1,44 @@
+#include <DHT.h>
+#include <DHT_U.h>
+
+
+
+
+#define DHTPIN A1 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+
+// Conecte pino 1 do sensor (esquerda) ao +5V
+// Conecte pino 2 do sensor ao pino de dados definido em seu Arduino
+// Conecte pino 4 do sensor ao GND
+// Conecte o resistor de 10K entre pin 2 (dados) 
+// e ao pino 1 (VCC) do sensor
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  dht.begin();
+}
+
+void loop() 
+{
+  // A leitura da temperatura e umidade pode levar 250ms!
+  // O atraso do sensor pode chegar a 2 segundos.
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // testa se retorno é valido, caso contrário algo está errado.
+  if (isnan(t) || isnan(h)) 
+  {
+    Serial.println("Failed to read from DHT");
+  } 
+  else 
+  {
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.print(" %t");
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.println(" *C");
+  }
+}
