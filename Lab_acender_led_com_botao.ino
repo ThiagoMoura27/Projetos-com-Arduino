1ffaffc0c9d33fff195f0d5b9e2e66c28ca61f74
Thiago Moura <thiago.uapi@gmail.com>
2018-02-13 09:56:37 -0300

    Add files via upload

---
+++ Lab_acender_led_com_botao.ino
@@ -0,0 +1,27 @@
+
+const int led = 8;   //constante led refere-se ao pino digital 8.
+const int botao = 2; //constante botão refere-se ao pino digital 2.
+ 
+//Variável que conterá os estados do botão (0 LOW, 1 HIGH).
+int estadoBotao = 0;
+ 
+//Método setup, executado uma vez ao ligar o Arduino.
+void setup() {
+  pinMode(led,OUTPUT);  //Definindo pino digital 8 como de saída.
+  pinMode(botao,INPUT); //Definindo pino digital 2 como de entrada. 
+}
+ 
+//Método loop, executado enquanto o Arduino estiver ligado.
+void loop() {  
+  //Lendo o estado do pino 2, constante botao, e atribuindo 
+  //o resultado a variável estadoBotao.
+  estadoBotao = digitalRead(botao);          
+   
+  //Verificando o estado do botão para definir se acenderá ou
+  //apagará o led.  
+  if (estadoBotao == HIGH) {
+    digitalWrite(led,HIGH); //Botão pressionado, acende o led.
+  } else {
+    digitalWrite(led,LOW);  //Botão não pressionado, apaga o led.    
+  }       
+}
