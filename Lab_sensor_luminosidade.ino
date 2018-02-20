61e36b9f3d5a713a5f8b78baad8ff1600c4afbe1
Thiago Moura <thiago.uapi@gmail.com>
2018-02-20 09:46:22 -0300

    Add files via upload

---
+++ Lab_sensor_luminosidade.ino
@@ -0,0 +1,28 @@
+int portaLed = 10; //Porta a ser utilizada para ligar o led  
+int portaLDR = A5; //Porta analógica utilizada pelo LDR  
+  
+void setup()  
+{  
+  Serial.begin(9600);
+  pinMode(portaLed, OUTPUT); //Define a porta do Led como saída  
+}  
+   
+void loop()  
+{  
+  int estado = analogRead(portaLDR);  //Lê o valor fornecido pelo LDR  
+
+   Serial.println(estado);
+  // Caso o valor lido na porta analógica seja maior do que 
+  // 800, acende o LED  
+  // Ajuste o valor abaixo de acordo com o seu circuito  
+  if (estado > 1000)    
+  {  
+    digitalWrite(portaLed, HIGH);  
+    Serial.println("Luz acesa!");
+  }  
+  else  //Caso contrário, apaga o led  
+  {  
+    digitalWrite(portaLed, LOW);  
+    Serial.println("Luz apagada");
+  }  
+}  
